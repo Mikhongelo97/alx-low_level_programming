e43e7d00229a5f24c81d521cadbed9d998f01c84
Mikhongelo97 <mikhomkhantshwa@gmail.com>
2023-05-06 05:46:16 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef main_h
 #define main_h
+#include <stdio.h>
+int _putchar(char c);
 char *_strncpy(char *dest, char *src, int n);
-int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
