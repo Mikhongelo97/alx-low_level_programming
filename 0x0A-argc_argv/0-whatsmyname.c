02914505d3d68a8fbf4079cdc62a3d6f2d606d3f
Mikhongelo97 <mikhomkhantshwa@gmail.com>
2023-05-12 03:05:56 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: 0 if successful
+ */
+int main(int argc, char **argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
