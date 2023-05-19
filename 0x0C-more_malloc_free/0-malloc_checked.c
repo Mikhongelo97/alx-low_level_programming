eeeea6985645e129eb5bff393f6134af70153850
Mikhongelo97 <mikhomkhantshwa@gmail.com>
2023-05-19 06:08:47 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,42 +2,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * *string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: limit of s2
- * Return: pointer to new space in memory or null
+ * malloc_checked - allocates memory using malloc
+ * @b: size to be allocated
+ * Return: pointer to allocated memory or exit with 98
  **/
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-unsigned int i, k, e;
-int coun1 = 0;
-char *q;
-if (s1 == NULL)
+void *a;
+a = malloc(b);
+if (a == NULL)
 {
-s1 = "";
+exit(98);
 }
-if (s2 == NULL)
-{
-s2 = "";
-}
-for (i = 0; s1[i] != '\0'; i++)
-{
-coun1 = coun1 + 1
-}
-q = malloc(sizeof(*q) * (coun1 + n + 1));
-if (q == NULL)
-{
-return (NULL);
-}
-for (k = 0; s1[k] != '\0'; k++)
-{
-q[k] = s1[k];
-}
-for (e = 0; s2[e] != '\0' && e < n; e++, k++)
-{
-q[k] = s2[e];
-}
-q[k] = '\0';
-return (q);
+return (a);
 }
